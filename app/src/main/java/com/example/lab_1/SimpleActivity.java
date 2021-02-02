12e7a39de5e48477f03ff9e8a2148118b485e0ba
Anusha <anusha.nagella@yahoo.com>
2021-02-01 20:55:02 -0500

    https://github.com/AnushaN818/mobileappdev.git

--- app/src/main/java/com/example/lab_1/SimpleActivity.java
+++ app/src/main/java/com/example/lab_1/SimpleActivity.java
@@ -11,17 +11,37 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SimpleActivity extends AppCompatActivity {
 
     final String LOG_STR = "Help!";
-
+    final String ON_CREATE = "in OnCreate!";
+    //private final String C_TAG = "ON_CREATE";
+    private final String S_TAG = "ON_START";
+    private final String R_TAG = "ON_RESUMED";
+    private final String P_TAG = "ON_PAUSED";
+    private final String ST_TAG = "ON_STOP";
+    private final String D_TAG = "ON_DESTROY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(ON_CREATE, "Started onCreate");
+
         setContentView(R.layout.activity_simple);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Button additions
+        Button toastButton = findViewById(R.id.t_button);
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_LONG).show();
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +49,33 @@ public class SimpleActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Log.i(LOG_STR,  "I'm melting!");
+                Log.i(LOG_STR, "I'm melting!");
             }
         });
     }
+        @Override
+        public void onStart() {
+            super.onStart();
+            Log.i(S_TAG,"Stopped to paused");
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            Log.i(R_TAG, "paused to resumed");
+        }
+        @Override
+        public void onPause(){
+            super.onPause();
+            Log.i(P_TAG, "resumed to paused");
+        }
+        @Override
+        public void onStop() {
+            super.onStop();
+            Log.i(ST_TAG, "paused to stop");
+        }
+        @Override
+        public void onDestroy(){
+            super.onDestroy();
+            Log.i(D_TAG, "stop to destroyed");
+        }
 }
